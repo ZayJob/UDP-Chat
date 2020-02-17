d4648202e953b76c2f8d786443dc167325669da0
Vlad Bubeniuk <ZayByst@mail.ru>
2020-02-17 04:01:54 +0300

    Check on pycodestyle and final change

--- client.py
+++ client.py
@@ -1,14 +1,16 @@
-import socket, threading, time
+import socket
+import threading
+import time
 
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 0
 
+
 class Client:
-    def __new__(cls,*args, **kwargs):
+    def __new__(cls, *args, **kwargs):
         """Singleton pattern"""
-        
-        if not hasattr(cls,'_inst'):
+        if not hasattr(cls, '_inst'):
             cls._inst = super(Client, cls).__new__(cls)
         return cls._inst
 
@@ -21,34 +23,34 @@ class Client:
         self.s = None
         self.rT = None
         self.join = False
-        
-    def client_config(self, AddressFamily=None , SocketKind=None) -> None:
+
+    def client_config(self, AddressFamily=None, SocketKind=None) -> None:
         """Confing for setting work client"""
         self._configurate_socket(AddressFamily, SocketKind)
         self._configurate_threading()
-    
+
     def _configurate_threading(self) -> None:
         """Setting threading"""
         self.rT = threading.Thread(target=self._receving_data, args=("RecvThread", self.s))
         self.rT.start()
-    
+
     def _configurate_socket(self, AddressFamily, SocketKind) -> None:
         """Setting socket, change UPD on TCP"""
-        if AddressFamily == None and SocketKind == None:
+        if AddressFamily is None and SocketKind is None:
             self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         else:
             self.s = socket.socket(AddressFamily, SocketKind)
-            
+
         self.s.bind((HOST, PORT))
         self.s.setblocking(0)
-    
+
     def _crypted(self, data: str) -> str:
         """Crypted string, symmetric method"""
         crypt_data = ""
         for char in data:
-            crypt_data += chr(ord(char)^self.key)
+            crypt_data += chr(ord(char) ^ self.key)
         return crypt_data
-    
+
     def _decrypted(self, data: str) -> str:
         """Decrypted string, symmetric method"""
         decrypt_data = ""
@@ -57,10 +59,10 @@ class Client:
             if char == ":":
                 flag = True
                 decrypt_data += char
-            elif flag == False or char == " ":
+            elif not flag or char == " ":
                 decrypt_data += char
             else:
-                decrypt_data += chr(ord(char)^self.key)
+                decrypt_data += chr(ord(char) ^ self.key)
         return decrypt_data
 
     def _receving_data(self, name: str, socket) -> None:
@@ -68,20 +70,55 @@ class Client:
         while True:
             try:
                 while True:
-                    data ,address = socket.recvfrom(1024)
-                    
+                    data, address = socket.recvfrom(1024)
+
                     decrypt_data = self._decrypted(data)
-                    print(decrypt_data)
-                    
+
+                    new_decrypt_data = self._confirmation(decrypt_data, address)
+
+                    print(new_decrypt_data)
+
                     time.sleep(0.2)
             except Exception as ex:
                 pass
+
+    def _confirmation(self, decrypt_data: str, address: tuple) -> str:
+        """Confirm send data"""
+        array_data = decrypt_data.split(" ")
+
+        if array_data[-1] == "confirm":
+            self.s.sendto(("OK " + str(array_data[-2])).encode("utf-8"), self.server)
+
+            return " ".join(array_data[:(len(array_data) - 2)])
+        return " ".join(array_data)
+
+    def _arg_parser(self, data: str) -> int:
+        """Search args in data, if not find args, generate exception"""
+        try:
+            array_data = data.split(" ")
+
+            if array_data[0] == "createGroup":
+                return (2, " ".join(array_data[1::]))
+            elif array_data[0] == "showClients":
+                return (3, " ".join(array_data[1::]))
+            elif array_data[0] == "showGroups":
+                return (4, " ".join(array_data[1::]))
+            elif array_data[0] == "nameGroup":
+                return (1, " ".join(array_data[1::]))
+            else:
+                raise
+        except Exception as ex:
+            return (0, data)
 
     def _join_server(self) -> None:
         """Send data after entry global chat"""
         data_crypted = self._crypted("=> join chat ")
         self.s.sendto(("\nName: {0} " + data_crypted).format(self.name_client).encode("utf-8"), self.server)
         self.join = True
+
+    def _left_server(self) -> None:
+        """Send data after exit global chat"""
+        self.s.sendto(("\nName: {0}  <= left chat ").format(self.name_client).encode("utf-8"), self.server)
 
     def _send_data_all_clients(self, data: str) -> None:
         """Send data to server"""
@@ -90,35 +127,14 @@ class Client:
         else:
             print("Incorrect data")
 
-    def _left_server(self) -> None:
-        """Send data after exit global chat"""
-        self.s.sendto(("\nName: {0}  <= left chat ").format(self.name_client).encode("utf-8"), self.server)
-        
-    def _arg_parser(self, data: str) -> int:
-        """Serch args in data, if not find args, generate exceptoin"""
-        try:
-            array_data = data.split(" ")
-            if array_data[0] == "createGroup":
-                return (None, 2, " ".join(array_data[1::]))
-            if array_data[0] == "showClients":
-                return (None, 3, " ".join(array_data[1::]))
-            if array_data[0] == "showGroups":
-                return (None, 4, " ".join(array_data[1::]))
-            if array_data[0] == "nameGroup":
-                return (None, 1, " ".join(array_data[1::]))
-            else:
-                raise
-        except Exception as ex:
-            return (None, 0, data)
-
-    def _show_groups(self):
+    def _show_groups(self) -> None:
         """Send command on server"""
         self.s.sendto(("showGroups").encode("utf-8"), self.server)
 
     def _show_clients(self) -> None:
         """Send command on server"""
         self.s.sendto(("showClients").encode("utf-8"), self.server)
-    
+
     def _create_group(self, data: str) -> None:
         """Send command on server"""
         self.s.sendto(("createGroup" + " " + data).encode("utf-8"), self.server)
@@ -126,22 +142,22 @@ class Client:
     def _send_data_to_group(self, data: str) -> None:
         """Send commant on server"""
         self.s.sendto(("nameGroup" + " " + data).encode("utf-8"), self.server)
-   
+
     def start(self) -> None:
         """Stream processing on client"""
         self.name_client = self._crypted(self.name_client)
-        
+
         while True:
-            if self.join == False:
+            if not self.join:
                 self._join_server()
             else:
                 try:
                     data = input()
-                    
-                    id_group, num_action, data = self._arg_parser(data)
+
+                    num_action, data = self._arg_parser(data)
 
                     crypt_data = self._crypted(data)
-                    
+
                     if num_action == 0:
                         self._send_data_all_clients(crypt_data)
                     elif num_action == 1:
@@ -152,7 +168,7 @@ class Client:
                         self._show_clients()
                     elif num_action == 4:
                         self._show_groups()
-                    
+
                     time.sleep(0.2)
                 except Exception as ex:
                     self._left_server()
@@ -167,12 +183,11 @@ if __name__ == "__main__":
         print("INIT CLIENT")
         name_client = input("ENTER NAME: ")
         client = Client(HOST, PORT, name_client)
-        
+
         print("CONFIGURATE CLIENT")
         client.client_config()
-        
+
         print("START CLIENT")
         client.start()
-
     except Exception as ex:
         print("STOP CLIENT - {0}".format(ex))
--- server.py
+++ server.py
@@ -1,14 +1,15 @@
-import socket, time
+import socket
+import time
 
 
 HOST = socket.gethostbyname(socket.gethostname())
 PORT = 9090
 
+
 class Server:
     def __new__(cls, *args, **kwargs):
         """Singleton pattern"""
-        
-        if not hasattr(cls,'_inst'):
+        if not hasattr(cls, '_inst'):
             cls._inst = super(Server, cls).__new__(cls)
         return cls._inst
 
@@ -18,20 +19,20 @@ class Server:
         self.key = key
         self.clients = []
         self.s = None
-    
-    def server_config(self, AddressFamily=None , SocketKind=None) -> None:
+
+    def server_config(self, AddressFamily=None, SocketKind=None) -> None:
         """Confing for setting work server"""
         self._configurate_socket()
-    
-    def _configurate_socket(self, AddressFamily=None , SocketKind=None) -> None:
+
+    def _configurate_socket(self, AddressFamily=None, SocketKind=None) -> None:
         """Setting socket, change UPD on TCP"""
-        if AddressFamily == None and SocketKind == None:
+        if AddressFamily is None and SocketKind is None:
             self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         else:
             self.s = socket.socket(AddressFamily, SocketKind)
-        
+
         self.s.bind((self.host, self.port))
-    
+
     def _connection_processing(self, data: str, address: tuple) -> bool:
         """Check client on list clients"""
         address_server = None
@@ -39,45 +40,46 @@ class Server:
         data_decrypt = self._decrypted(data)
 
         array_data = data_decrypt.split(" ")
-        
+
         if array_data[0] == "\nName:":
             address_server = [address, array_data[1]]
-            
+
             if address_server not in self.clients:
                 self.clients.append(address_server)
- 
+
         print("IP - {0} | PORT CLIENT - {1}".format(address[0], address[1]))
         return True
-    
-    def _arg_parser(self, data: str) -> int:
+
+    def _confirmation(self, array_data: list, address: tuple) -> None:
+        """Confirm send data"""
+        self.s.sendto("Delivered".encode("utf-8"), ("", int(array_data[1])))
+
+    def _arg_parser(self, data: str, address: tuple) -> tuple:
         """Serch args in data, if not find args, generate exceptoin"""
         try:
-            array_data = data.decode("utf-8").split()
+            array_data = data.decode("utf-8").split(" ")
 
-            if array_data[0] == "createGroup":
-                return (None, 2, " ".join(array_data[1::]))
+            if array_data[0] == "OK":
+                self._confirmation(array_data, address)
+                return (5, None)
+            elif array_data[0] == "createGroup":
+                return (2, " ".join(array_data[1::]))
             elif array_data[0] == "showClients":
-                return (None, 3, " ".join(array_data[1::]))
+                return (3, " ".join(array_data[1::]))
             elif array_data[0] == "showGroups":
-                return (None, 4, " ".join(array_data[1::]))
+                return (4, " ".join(array_data[1::]))
             elif array_data[0] == "nameGroup":
-                return (None, 1, " ".join(array_data[1::]))
+                return (1, " ".join(array_data[1::]))
             else:
                 raise
         except Exception as ex:
-            return (None, 0, data)
-    
-    def _print_data(self, data: str) -> None:
-        """Data processing and output in console"""
-        server_time = time.strftime("%Y-%M-%D-%H-%M-%S", time.localtime())
-        
-        print("Time: {0} | Data: {1}".format(server_time, data.decode("utf-8")))
+            return (0, data)
 
     def _crypted(self, data: str) -> str:
         """Crypted string, symmetric method"""
         crypt_data = ""
         for char in data:
-            crypt_data += chr(ord(char)^self.key)
+            crypt_data += chr(ord(char) ^ self.key)
         return crypt_data
 
     def _decrypted(self, data: str) -> str:
@@ -88,11 +90,17 @@ class Server:
             if char == ":":
                 flag = True
                 decrypt_data += char
-            elif flag == False or char == " ":
+            elif not flag or char == " ":
                 decrypt_data += char
             else:
-                decrypt_data += chr(ord(char)^self.key)
+                decrypt_data += chr(ord(char) ^ self.key)
         return decrypt_data
+
+    def _print_data(self, data: str) -> None:
+        """Data processing and output in console"""
+        server_time = time.strftime("%Y-%M-%D-%H-%M-%S", time.localtime())
+
+        print("Time: {0} | Data: {1}".format(server_time, data.decode("utf-8")))
 
     def _send_data_all_clients(self, data: str, address: tuple) -> None:
         """Send data all clients"""
@@ -102,56 +110,59 @@ class Server:
             if address != client:
                 self.s.sendto(data, client)
 
-    def _send_information_about_clients(self, address: tuple) -> None:
+    def _send_data_about_clients(self, address: tuple) -> None:
         """Send data all clients"""
-        data = None
+        data = ""
         for index, client in enumerate(self.clients):
             if address != client[0]:
                 data += " ".join([str(index), str(client[0][0]), str(client[0][1]), str(client[1]), "\n"])
 
         self.s.sendto(data.encode("utf-8"), address)
-        
+
     def _create_group(self, data: str, address: tuple) -> None:
         """Add group to file"""
         with open("Groups.txt", "r") as file_r:
             array_groups = file_r.readlines()
-            
+
             with open("Groups.txt", "w") as file_w:
-                line = " ".join([str(address[1]), data, "\n"])                
+                line = " ".join([("\n" + str(address[1])), data])
                 array_groups.append(line)
-                
+
                 file_w.writelines(array_groups)
-    
-    def _send_information_about_groups(self, address: tuple) -> None:
+
+    def _send_data_about_groups(self, address: tuple) -> None:
         """Send information about group to client"""
         with open("Groups.txt", "r") as file_r:
             array_groups = file_r.readlines()
             data = ""
-            
+
             for index, group in enumerate(array_groups):
                 array_data = group.split(" ")
-                
+
                 if int(array_data[0]) != int(address[1]):
                     continue
 
                 data += " ".join([array_data[1], " ".join(array_data[2:]), "\n"])
 
-            self.s.sendto(data.encode("utf-8"), address)    
+            self.s.sendto(data.encode("utf-8"), address)
 
     def _send_data_to_group(self, data: str, address: tuple) -> None:
         """Send data to group"""
         with open("Groups.txt", "r") as file_r:
             array_groups = file_r.readlines()
             array_data_msg = data.split(" ")
-            
+
             for index, group in enumerate(array_groups):
                 array_data = group.split(" ")
-                
+
                 if int(array_data[0]) == int(address[1]) and array_data[1] == array_data_msg[0]:
                     clients = array_data[2:]
-                    
+
                     for client in clients:
                         msg = " ".join(array_data_msg[1:])
+
+                        if len(clients) == 1:
+                            msg = " ".join([msg, str(address[1]), "confirm"])
 
                         self.s.sendto(msg.encode("utf-8"), ("", int(client)))
 
@@ -163,11 +174,11 @@ class Server:
 
                 connect = self._connection_processing(data, address)
 
-                if connect == True:
+                if connect:
                     self._print_data(data)
 
-                    id_chat, num_action, data = self._arg_parser(data)
-                    
+                    num_action, data = self._arg_parser(data, address)
+
                     if num_action == 0:
                         self._send_data_all_clients(data, address)
                     elif num_action == 1:
@@ -175,9 +186,11 @@ class Server:
                     elif num_action == 2:
                         self._create_group(data, address)
                     elif num_action == 3:
-                        self._send_information_about_clients(address)
+                        self._send_data_about_clients(address)
                     elif num_action == 4:
-                        self._send_information_about_groups(address)
+                        self._send_data_about_groups(address)
+                    elif num_action == 5:
+                        pass
                 else:
                     raise ConnectionError
             except Exception as ex:
@@ -190,12 +203,11 @@ if __name__ == "__main__":
     try:
         print("INIT SERVER")
         server = Server(HOST, PORT)
-        
+
         print("CONFIGURATE SERVER")
         server.server_config()
-        
+
         print("START SERVER")
         server.start()
-    
     except Exception as ex:
         print("STOP SERVER - {0}".format(ex))
